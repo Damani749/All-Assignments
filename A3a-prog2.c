6dfd6cc372cf5d6043f40de44936bf2044b559fd
Ramya Damani <damani.ramya309@gmail.com>
2024-03-13 18:14:22 +0530

    Assign3a: printing a table of given number

---
+++ A3a-prog2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int num, count = 1;
+	printf("Enter a number whose table you want to print.\n");
+	scanf("%d",&num);
+
+	while (count < 11)
+	{
+		printf("%d * %d = %d\n",num, count, num*count);
+		count ++;
+	}
+	return 0;
+}
