1ba21227cfd3e0476119c756adbad017034e7767
Ramya Damani <damani.ramya309@gmail.com>
2024-03-14 15:54:40 +0530

    C-Assign-6: printing unique combinations of a given number using while loop

---
+++ A3a-prog7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int num, count = 1;
+	printf("Enter the number whose unique multiplication pairs you want.\n");
+	scanf("%d", &num);
+
+	while (count <= num/2)
+	{	
+		if (num % count == 0)
+		{
+			if (count < num/count)
+				printf("%d * %d = %d\n", count, num/count, num);
+		}
+		count++;
+	}
+	return 0;
+}
+
