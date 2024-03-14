9d683ff9b97868ebe0b9781c4aa4da4a35ea10a8
Ramya Damani <damani.ramya309@gmail.com>
2024-03-14 15:23:49 +0530

    C-Assign-6: Factors of a given number using while loop

---
+++ A3a-prog6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int num,count = 1;
+	printf("Enter the number whose factors you want.\n");
+	scanf("%d", &num);
+	
+	printf("The factors are: ");
+	while (count <= num/2)
+	{
+		if (num % count == 0)
+			printf("%d, ",count);
+		count ++;
+	}
+	printf("\n");
+
+	return 0;
+}
