939410d153b7cb8f4f3b792c4bab08d8cca4976f
Ramya Damani <damani.ramya309@gmail.com>
2024-03-14 17:02:40 +0530

    C-Assign-4: Factorial of a given number using while loop

--- A3a-prog4.c
+++ A3a-prog4.c
@@ -5,13 +5,13 @@ int main()
 	int num,fact = 1;
 	printf("Enter a given number whose factorial you want.\n");
 	scanf("%d", &num);
-
+	
+	// Using while loop
 	while(num != 0)
 	{
 		fact = fact * num;
 		num --;
 	}
-
 	printf("The factorial of a given number is %d.\n",fact);
 	return 0;
 }
