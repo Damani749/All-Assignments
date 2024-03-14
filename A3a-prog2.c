475e916dae61bd6b475e59323308722d2c9c465f
Ramya Damani <damani.ramya309@gmail.com>
2024-03-14 17:05:55 +0530

    C-Assign-2: Printing a table of given number

--- A3a-prog2.c
+++ A3a-prog2.c
@@ -5,7 +5,8 @@ int main()
 	int num, count = 1;
 	printf("Enter a number whose table you want to print.\n");
 	scanf("%d",&num);
-
+	
+	// Using while loop
 	while (count < 11)
 	{
 		printf("%d * %d = %d\n",num, count, num*count);
