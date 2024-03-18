cc05bbcce3fd17def952f2563b519220a4f4a80f
Ramya Damani <damani.ramya309@gmail.com>
2024-03-18 23:59:08 +0530

    CAssign3: Second digit pattern

---
+++ A3-prog22d.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int i, j, num = 1;
+	for (i = 1; i < 6; i++)
+	{
+		num = 5;
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d", num);
+			num = num -1;
+		}
+	printf("\n");
+	}
+	return 0;
+}
