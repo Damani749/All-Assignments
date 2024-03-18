96ede6723afa1c4d4b59812738d678158d09d35c
Ramya Damani <damani.ramya309@gmail.com>
2024-03-19 00:50:03 +0530

    CAssign3: Pascal's Trainagle

---
+++ A3-prog23.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int factorial(int n) 
+{
+    if (n <= 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int binomial_Coefficient(int n, int r)
+{
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+void Pascal_Triangle(int rows) 
+{
+    for (int i = 0; i < rows; i++) 
+	{
+        for (int j = 0; j <= i; j++) 
+            printf("%5d ", binomial_Coefficient(i, j));
+
+        printf("\n");
+    }
+}
+
+int main() 
+{
+    int rows;
+    printf("Enter the number of rows for Pascal's Triangle: ");
+    scanf("%d", &rows);
+
+    Pascal_Triangle(rows);
+    return 0;
+}
