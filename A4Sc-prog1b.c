6b869e6e2fd93f156fa52fba21dcf3987c70f162
Ramya Damani <damani.ramya309@gmail.com>
2024-03-20 17:30:19 +0530

    CAssign4: Printing sum and product of two numbers without global declaration using pointers

---
+++ A4Sc-prog1b.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int add (int *n1, int *n2);
+int mul (int *n1, int *n2);
+
+int main()
+{
+	int num1, num2, result;
+	printf("Enter two numbers to get its product and Sum.\n");
+	scanf("%d %d", &num1, &num2);
+	result = add(&num1, &num2);
+	printf("The result of addition is %d.\n",result);
+	result = mul(&num1, &num2);
+	printf("The result of product  is %d.\n",result);
+	return 0;
+}
+
+int add(int *n1, int *n2)
+{
+	return *n1+ *n2;
+}
+
+int mul(int *n1, int *n2)
+{
+	return *n1 * *n2;
+}
